d98bd2ca38b5ef0cae7576556bcda1919e4b3cb6
CarlosStrider120 <sherifinho.augusto@gmail.com>
2022-10-31 01:46:58 -0300

    transforming into an API with authentication

--- modules/server.js
+++ modules/server.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const path = require('path');
 const UserModel = require('../src/database/models/user.model');
 
@@ -6,11 +9,27 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, '../src/views')));
 
+const checkToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader.split(" ")[1];
+    if (token) {
+        try {
+            const secret = process.env.SECRET;
+            jwt.verify(token, secret);
+        } catch (error) {
+            res.status(400).send('Token inválido!');
+        }
+        next();
+    } else {
+        res.status(401).json('Acesso negado!');
+    }
+
+}
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../src/views', 'index.html'));
+    res.status(200).send('API de autenticação');
 });
 
 app.get('/register', (req, res) => {
@@ -21,39 +40,65 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/views', 'login.html'));
 });
 
-app.post('/register', async (req, res) => {
+app.get('/users', async (req, res) => {
     try {
-        const { email } = req.body
-        const userExist = await UserModel.findOne({ email: email });
-        if (userExist) {
-            res.redirect('/register');
-        } else {
-            const user = await UserModel.create(req.body);
-            res.redirect('/login');
-        }
+        const users = await UserModel.find({});
+        res.status(200).json(users);
     } catch (error) {
-        res.send(error.message);
+        res.status(500).send('Não foi possível listar os usuários!');
+    }
+});
+
+app.get('/users/:id', checkToken, async (req, res) => {
+    const id = req.params.id;
+    const user = await UserModel.findById(id, '-password');
+    if (user) {
+        return res.status(200).json(user);
+    }
+    res.status(404).send('Usuário não encontrado!');
+});
+
+app.post('/register', async (req, res) => {
+
+    const { username, email, password } = req.body
+    const userExist = await UserModel.findOne({ email: email });
+    if (userExist) {
+        res.status(422).send('Usuário com esse e-mail já está cadastrado');
+    }
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+    const user = new UserModel({
+        username,
+        email,
+        password: passwordHash
+    });
+    try {
+        await user.save();
+        res.status(201).send("Usuário criado com sucesso!");
+    } catch (error) {
+        return res.status(500).json(error);
     }
 });
 
 app.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await UserModel.findOne({ email: email });
-        if (user) {
-            const passwordMatch = user.password == password;
-            if (passwordMatch) {
-                res.redirect('/');
-            } else {
-                res.redirect('/login');
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email: email });
+    if (user) {
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (passwordMatch) {
+            try {
+                const secret = process.env.SECRET;
+                const token = jwt.sign({
+                    id: user._id
+                }, secret);
+                return res.status(200).json({ message: 'Autenticação realizada com sucesso!', token });
+            } catch (error) {
+                res.status(500).send(error);
             }
-
-        } else {
-            res.redirect('/login');
         }
-    } catch (error) {
-        res.send(error.message);
+        res.status(422).send('Senha inválida');
     }
+    res.status(404).send("Usuário não existe!");
 });
 
 const port = 3000;
